9162e1b7ef674276026b86dc5121fc5a4b686cc9
sebasjd <d.jsebastian@hotmail.com>
2022-10-03 22:38:10 -0300

    efecto a  la card

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const init = () => card.innerHTML = `<p class="start">Para continuar introduzca un número en la casilla de ID y presione SEND</p>`
+init()
 
 const fetchPokemon = async(pokeID) => {
-    const response = await fetch(baseURL + pokeID + `/`);
-    const data = await response.json();
-    // console.log(data);
-    return data
+    try {
+        const response = await fetch(baseURL + pokeID + `/`);
+        const data = await response.json();
+        // console.log(response);
+        return data
+    } catch { alert("Este ID no coincide con ningún pókemon") }
 }
 
 
 
+
 const renderPockemon = async(pokeID) => {
-        const pokemon = await fetchPokemon(pokeID);
+        const pokemon = await fetchPokemon(pokeID)
         const { id, name, types, height, weight, sprites } = pokemon
-        console.log(sprites);
 
         card.innerHTML =
             `<h4 id="id">#${id}</h4>
@@ -27,20 +31,30 @@ const renderPockemon = async(pokeID) => {
     <h3 id="height">Altura:<span>${height*10} Cm.</span></h3>
     <h3 id="weight">Peso:<span>${weight/10} Kgs.</span></h3>
     `
+    
 }
 
-const isEmpty=()=>{
-     alert("Debe introducir un ID para continuar");
+const vanish = ()=>{
+    card.classList.remove("appear")
+    card.classList.add("vanish")
+}
+const appear = ()=>{
+    card.classList.remove("vanish");
+    card.classList.add("appear");
+}
+const isEmpty= ()=>alert("Debe introducir un ID para continuar");
 
-}
-const isWrong=()=>{
-    alert("No existen tantos pokemons");
-}
+const isWrong= ()=>alert("No existen tantos pókemons");
+
 btn.addEventListener("click", button);
 
+const mostrar =()=>{}
 function button(e) {
     e.preventDefault();
     let pokeID = input.value.trim();
-    pokeID=="" || pokeID==0?isEmpty():pokeID>905?isWrong():renderPockemon(pokeID)
+    vanish()
+    pokeID=="" || pokeID==0?isEmpty():pokeID>20000?isWrong():setTimeout(renderPockemon,200,pokeID);
+    setTimeout(appear, 200);
     input.value = "";
+    
 }
